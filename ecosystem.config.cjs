fb54d4f9a1cf1b83e1d6dd0f1b0babc7a67b8094
Afraz Malik <afrazmalik321@gmail.com>
2026-09-04 09:38:39 +0500

    Cool thing?

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'biselahore.com',
-      script: 'npm',
+      script: 'node_modules/next/dist/bin/next',
       args: 'start',
       instances: '1',
       exec_mode: 'fork',
